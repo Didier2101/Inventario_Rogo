6dab379768f33404379ad0aa76535654614a2194
didier chavez <didierchavez2101@gmail.com>
2024-08-12 10:50:33 -0500

    de nuevo

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-const pool = require("./database");
+const pool = require("./src/database"); // Asegúrate de que la ruta a database.js sea correcta
 const path = require("path");
 
-const empleadosRouter = require("./routes/empleadoRouter");
-const usuariosRouter = require("./routes/usuarioRouter");
-const clientesRouter = require("./routes/clientesRouter");
-const proveedoresRouter = require("./routes/proveedorRouter");
-const puntosVentasRouter = require("./routes/puntoVentaRouter");
-const bodegasRouter = require("./routes/bodegaRouter");
-const productosRouter = require("./routes/productoRouter");
-const cargosRouter = require("./routes/cargoRouter");
+const empleadosRouter = require("./src/routes/empleadoRouter");
+const usuariosRouter = require("./src/routes/usuarioRouter");
+const clientesRouter = require("./src/routes/clientesRouter");
+const proveedoresRouter = require("./src/routes/proveedorRouter");
+const puntosVentasRouter = require("./src/routes/puntoVentaRouter");
+const bodegasRouter = require("./src/routes/bodegaRouter");
+const productosRouter = require("./src/routes/productoRouter");
+const cargosRouter = require("./src/routes/cargoRouter");
 
 const app = express();
 const port = 4000;
@@ -28,12 +28,12 @@ app.use(bodegasRouter);
 app.use(productosRouter);
 app.use(cargosRouter);
 
-// Servir archivos estáticos de React
-app.use(express.static(path.join(__dirname, "dist")));
+// Servir archivos estáticos del frontend
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // Ruta catch-all para servir index.html en caso de una ruta desconocida
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
 
 app.listen(port, async () => {
